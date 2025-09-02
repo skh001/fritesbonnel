a0684acfaed1e18cdb304a2d8d12b36b0a988ab4
Sofiane Khenchelaoui <sofianekhenchelaoui1@gmail.com>
2025-09-02 11:33:47 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/fritesbonnel/', // <--- Ajoutez cette ligne
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
