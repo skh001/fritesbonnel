0d0ee54a356d48bc15204f9b663ffb1eaabb4830
Sofiane Khenchelaoui <sofianekhenchelaoui1@gmail.com>
2025-09-02 11:47:32 +0200

    Déploiement final pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fritesbonnel/', // <--- Ajoutez cette ligne
+  base: '/fritesbonnel/', // <--- C'est la ligne la plus importante.
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
